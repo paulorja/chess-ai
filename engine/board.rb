50f433262b91b04cc76ea33cf42ab6fdd5c69403
paulo jansen <pauloo.jansen@gmail.com>
2019-01-23 02:42:32 -0200

    basic move

--- engine/board.rb
+++ engine/board.rb
@@ -19,4 +19,18 @@ class Board
     return @matrix[array_pos[0]][array_pos[1]]
   end
 
+  def movement(movement)
+    return false unless is_valid_movement? movement
+    from = BoardMapping::SQUARES[movement[0..1].to_sym]
+    to = BoardMapping::SQUARES[movement[2..3].to_sym]
+    @matrix[to[0]][to[1]] = @matrix[from[0]][from[1]]
+    @matrix[from[0]][from[1]] = 0
+  end
+
+  private
+
+  def is_valid_movement?(movement)
+    true
+  end
+
 end
